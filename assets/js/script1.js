82a22fe907e503bb8bee32c988ec1b4a3fac1093
FredericF <frederic.fauquembert@sfr.fr>
2018-06-25 15:23:55 +0200

    Script exo 1

---
+++ assets/js/script1.js
@@ -0,0 +1,10 @@
+//Déclaration de la fonction dessiner un cadre
+function dessine_cadre(id){
+  //définition de la bordure du cadre à 1px solide
+  id.style.border = '#00F 1px solid'
+}
+//Déclaration de la fonction retirer le cadre
+function efface_cadre(id){
+  //définition de la bordure du cadre à 0px
+  id.style.border = '0'
+}
