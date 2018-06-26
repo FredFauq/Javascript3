247197d52f25dd61048a48ee2befba3a99cb37ad
FredericF <frederic.fauquembert@sfr.fr>
2018-06-26 11:57:04 +0200

    Script exo 3

---
+++ assets/js/script3.js
@@ -0,0 +1,9 @@
+function colorG(){
+document.getElementById('text').style.color = 'green';
+}
+function colorR() {
+document.getElementById('text').style.color = 'red';
+}
+function colorB() {
+document.getElementById('text').style.color = 'blue';
+}
